438f1c9e5a592077ff72c9767d832e3e424dcd4d
Firdaus2929 <firdaushermawan29@gmail.com>
2021-12-15 14:21:42 +0700

    Pertemuan14

---
+++ app/src/main/java/com/informatika19100018/databarang/model/ResponseBarang.kt
@@ -0,0 +1,28 @@
+package com.informatika19100018.databarang.model
+
+import com.google.gson.annotations.SerializedName
+import java.sql.ClientInfoStatus
+
+data class ResponseBarang(
+
+    @field:SerializedName( "pesan")
+    val pesan: String? = null,
+
+    @field:SerializedName("data")
+    val data: List<DataItem?>? = null,
+
+    @field:SerializedName("status")
+    val status: Boolean? = null
+)
+
+data class DataItem(
+
+    @field:SerializedName("Nama_barang")
+    val namaBarang: String? = null,
+
+    @field:SerializedName("Id")
+    val id: String? = null,
+
+    @field:SerializedName("Jumlah_barang")
+    val jumlahBarang: String? =null
+)
